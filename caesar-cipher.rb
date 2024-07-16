0c322731e340d71976196eb136793f7d2363a6b6
Andrew Rowley <andrew.rowley160@outlook.com>
2024-07-16 11:30:31 -0400

    Add casear-cypher project

---
+++ caesar-cipher.rb
@@ -0,0 +1,26 @@
+def main()
+  print("Message: ")
+  message = gets
+
+  print("Shift: ")
+  shift_value = gets.to_i
+
+  puts caeser_cipher(message, shift_value)
+end
+
+def caeser_cipher(message, shift_value)
+  message.chars.map { |char| shift_letter(char, shift_value) }.join
+end
+
+def shift_letter(char, shift_value)
+  case char
+  when "A".."Z"
+    ((char.ord - "A".ord + shift_value) % 26 + "A".ord).chr
+  when "a".."z"
+    ((char.ord - "a".ord + shift_value) % 26 + "a".ord).chr
+  else
+    char
+  end
+end
+
+main
